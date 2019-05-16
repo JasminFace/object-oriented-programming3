2d03d70814f52ae0cf0c0360209e180f3a5f3ce5
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-16 14:07:48 -0400

    completed reinforcement

--- reinforcement.py
+++ reinforcement.py
@@ -1,14 +1,40 @@
+#EXERCISE 1
+
 class Task:
 
-    def __init__(self, job, due_date,):
-        self.job = job
+    def __init__(self, description, due_date):
+        self.description = description
         self.due = due_date
-        self.done = False
 
     def __str__(self):
-        return f"Description: {self.job} -- Due: {self.due}"
+        return f"Description: {self.description} -- Due: {self.due}"
+
+groceries = Task("Buy Food", "Tuesday")
+laundry = Task("Do laundry", "Next week")
+gym = Task("Go to the gym", "EVERYDAY")
+
+#EXERCISE 2
+
+class ToDoList:
+
+    def __init__(self):
+        self.tasks = []
+    
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def show_list(self):
+        print("----------------  STUFF TO DO  ----------------")
+        print("-----------------------------------------------")
+        for task in self.tasks:
+            print(f"- {task}")
 
 
-dishes = Task("Dishes", "NOW")
-print(dishes)
 
+getitdone = ToDoList()
+
+getitdone.add_task(gym)
+getitdone.add_task(laundry)
+getitdone.add_task(groceries)
+
+getitdone.show_list()
